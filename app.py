ce457639b50749a8806a7e43d47b7c3e049cd9e2
sonicpierre <pvirgaux@gmail.com>
2021-12-18 22:34:16 +0100

    Avance présentation

--- app.py
+++ app.py
@@ -7,12 +7,19 @@ db = bdd.connexion(st.secrets["textkey"], "michelin-4180a")
 ajout, dico_entree = ut.construction_form_sidebar()
 
 if ajout:
-    bdd.ecriture_data(db, 'Indicateur', dico_entree)
+    if (dico_entree['Name']!= ""):
+        bdd.ecriture_data(db, 'Indicateur', dico_entree)
+    else :
+        st.sidebar.error("Please enter the name of an indicator !")
 
-st.markdown('# Michelin Research Glossary')
+st.markdown('# Macro indicator research glossary')
+
+st.write('''We are doing more and more studies to understand and visualize the different economic macro indicators. 
+However, in order to avoid doing the same research several times and facing unnecessary problems this small application 
+has been set up to list everything that has been done on this subject.''')
 
 form_recherche = st.form("Recherche form")
-texte_a_chercher = form_recherche.text_input("What do you want ?")
+texte_a_chercher = form_recherche.text_input("What do you want")
 recherche = form_recherche.form_submit_button("Rechercher")
 
 if not recherche:
@@ -23,3 +30,6 @@ if recherche:
 
     for doc in docs:
         bdd.presentation_indicateur(doc.to_dict())
+
+    if(texte_a_chercher == ""):
+        bdd.lecture_data(db, "Indicateur")
--- gestion_BBD.py
+++ gestion_BBD.py
@@ -20,19 +20,25 @@ def presentation_indicateur(post):
     c = st.container()
     c.markdown('## ' + post["Name"])
     col1, col2 = c.columns(2)
-    col1.markdown("<strong>Etude associé : </strong>" + post["Associated Study"], unsafe_allow_html=True)
-    col1.markdown("<strong>Source : </strong>" + post["Source"], unsafe_allow_html=True)
-    col1.markdown("<strong>Frequence : </strong>" + post["Frequence"], unsafe_allow_html=True)
-    col2.markdown("<strong>Remark or issues aborded : </strong>" + post["Issues"], unsafe_allow_html=True)
-    col2.markdown("<strong>Contact : </strong>" + post["Contact"], unsafe_allow_html=True)
-    if(post["Video"]):
-        c.empty()
-        c.write("To have more information about this indicator you can see the following video :")
-        c.video(post["Explicative Source"])
-    else:
-        c.empty()
-        c.write("To have more information about this indicator you can see the following source :")
-        c.write(post["Explicative Source"])
+    if("Associated Study" in post):
+        col1.markdown("<strong>Etude associé : </strong>" + post["Associated Study"], unsafe_allow_html=True)
+    if("Source" in post):
+        col1.markdown("<strong>Source : </strong>" + post["Source"], unsafe_allow_html=True)
+    if("Frequence" in post):
+        col1.markdown("<strong>Frequence : </strong>" + post["Frequence"], unsafe_allow_html=True)
+    if("Issues" in post):
+        col2.markdown("<strong>Remark or issues aborded : </strong>" + post["Issues"], unsafe_allow_html=True)
+    if("Contact" in post):
+        col2.markdown("<strong>Contact : </strong>" + post["Contact"], unsafe_allow_html=True)
+    if("Video" in post) and ("Explicative Source" in post):
+        if(post["Video"]):
+            c.empty()
+            c.write("To have more information about this indicator you can see the following video :")
+            c.video(post["Explicative Source"])
+        else:
+            c.empty()
+            c.write("To have more information about this indicator you can see the following source :")
+            c.write(post["Explicative Source"])
 
 
 def ecriture_data(db, collection_name, dico_form):
